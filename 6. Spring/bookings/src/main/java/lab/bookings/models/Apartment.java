a1676c4836cd4cc3932e0fa28a3522894b8d1913
Bartosz Czarnecki <56341461+bartoszCzarnecki@users.noreply.github.com>
2022-05-03 21:49:56 +0200

    6. Spring

---
+++ 6. Spring/bookings/src/main/java/lab/bookings/models/Apartment.java
@@ -0,0 +1,55 @@
+package lab.bookings.models;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Apartment {
+    @javax.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private int capacity;
+
+    @OneToMany(mappedBy = "apartment")
+    private List<Booking> bookings;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+// getters & setters
+}
