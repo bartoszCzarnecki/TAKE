b3e0d746c9aeee875ef1855de8592a63a0e17426
Bartosz Czarnecki <56341461+bartoszCzarnecki@users.noreply.github.com>
2022-05-26 21:33:33 +0200

    8. ASP.NET MVC + Entity Framework (Code First)
    second

---
+++ 8. ASP.NET MVC + Entity Framework (Code First)/MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        public int Id { get; set; }
+        
+        [DataType("DateOnly")]
+        public DateTime Timestamp { get; set; }
+        
+        [Required]
+        [StringLength(140, MinimumLength = 5)]
+        public string Text { get; set; } = String.Empty;
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; } = new byte[0];
+    }
+}
